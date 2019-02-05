83117774a0038716f2346159fd08aa02ffc27a65
Marcelo Mello <marcelo.mello@liferay.com>
2019-02-05 18:02:39 -0300

    LPS-88530 Add DateFormatter

---
+++ Source/Util/DateUtil.swift
@@ -0,0 +1,31 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+
+import Foundation
+
+/**
+- Author: Marcelo Mello
+*/
+internal class DateUtil {
+	static let DATE_FORMAT: String = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+	static let DATE_ABBREVIATION: String = "UTC"
+	
+	static func formatDate(date: Date) -> Date {
+		let dateFormatter = DateFormatter()
+		dateFormatter.timeZone = TimeZone(abbreviation: DATE_ABBREVIATION)
+		dateFormatter.dateFormat = DATE_FORMAT
+		
+		return dateFormatter.date(from: dateFormatter.string(from: date)) ?? date
+	}
+}
